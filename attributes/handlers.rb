18daca1351ce6425a5169a536acb2becaab908cb
Tony F <tony@circumventures.com>
2016-01-08 10:55:19 -0800

    move stuff around

--- attributes/handlers.rb
+++ attributes/handlers.rb
@@ -2,15 +2,3 @@ default['monitor']['pagerduty_api_key'] = ''
 
 default['monitor']['graphite_address'] = nil
 default['monitor']['graphite_port'] = nil
-
-default['monitor']['hipchat_notifications'] = [
-  {
-    username: 'Sensu',
-    room: 'eComm - DevOps Team'
-  },
-  {
-    username: 'Sensu',
-    room: 'eComm - Frontend Dev Team'
-  }
-]
-default['monitor']['hipchat_token'] = 'qs44ZwTOb3G1r5y7xFguSHS2CQy6aZVf2N8e4G00'
