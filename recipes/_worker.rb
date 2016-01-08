667827f0b21552c77b187223b53f561797cfb1dc
Tony F <tony@circumventures.com>
2016-01-08 10:49:01 -0800

    added restart upon new hipchat handler creation

--- recipes/_worker.rb
+++ recipes/_worker.rb
@@ -86,8 +86,7 @@ if node['monitor']['hipchat_notifications']
           room: notification['room'],
           apitoken: node['monitor']['hipchat_token']
         )
-        notifies :create, 'ruby_block[sensu_service_trigger]', :delayed if
-        sensu_service_trigger
+        notifies :create, 'ruby_block[sensu_service_trigger]', :delayed if sensu_service_trigger
       end
   end
 end
